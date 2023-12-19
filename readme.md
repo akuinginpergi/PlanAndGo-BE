861b7950251e419ca2bc9d73dddefd1e1f85d249
addthoriq <alvarro.thoriq@gmail.com>
2023-12-20 02:35:25 +0700

    new

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Dokumentasi Postman API
+(https://www.postman.com/addthoriq/workspace/aku-ingin-pergi/collection/8172285-e5dea527-6afc-4eff-9559-d51519c6c16c?action=share&creator=8172285&active-environment=8172285-c2779bfa-6628-41ac-86bf-dbd4f85e4d11)
+
+# Install
+    npm install
+    npm run dev
---
+++ src/new-plan/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const  uuid = require('short-uuid')
+const { PrismaClient} = require('@prisma/client')
+let toCSV = require('json-2-csv')
+
+const prisma = new PrismaClient()
+const router = express.Router();
+
+router.get('/result-new-plan', async (req, res) => {
+  const result = await prisma.$queryRaw`SELECT * FROM pesananku`
+  res.send(result)
+})
+
+router.post('/store-new-plan', async (req, res) => {
+  const plan = req.body
+  const csv = await toCSV.json2csv(plan)
+  let id = uuid.generate()
+  // await prisma.$executeRaw`INSERT INTO pesananku_temp (id, kota_asal, kota_tujuan, tanggal_berangkat, tanggal_pulang, dana, tema) VALUES (${id}, ${plan.kota_asal}, ${plan.kota_tujuan}, to_date(${plan.tgl_berangkat}, 'YYYY/MM/DD'), to_date(${plan.tgl_pulang}, 'YYYY/MM/DD'), ${parseInt(plan.dana)}, ${plan.tema})`
+
+  console.log(csv)
+  res.send(csv)
+})
+
+router.post('/plan-it', async (req, res) => {
+  const plan = req.body
+  let id = uuid.generate()
+  await prisma.$executeRaw`INSERT INTO pesananku_temp (id, kota_asal, kota_tujuan, tanggal_berangkat, tanggal_pulang, dana, tema) VALUES (${id}, ${plan.kota_asal}, ${plan.kota_tujuan}, to_date(${plan.tgl_berangkat}, 'YYYY/MM/DD'), to_date(${plan.tgl_pulang}, 'YYYY/MM/DD'), ${parseInt(plan.dana)}, ${plan.tema})`
+  console.log(plan)
+  res.send(plan)
+})
+
+module.exports = router
