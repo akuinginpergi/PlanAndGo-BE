211960c4d60876ae271ffed07cfc328ab9c770be
addthoriq <alvarro.thoriq@gmail.com>
2023-12-20 02:43:21 +0700

    basic route setup and my plan

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ dotenv.config()
 const PORT = process.env.PORT
 
 const newPlan = require('./src/new-plan/index')
+const myPlan = require('./src/my-plan/index')
 app.use(cors());
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', newPlan)
+app.use('/', myPlan)
 
 app.listen(PORT, () => {
   console.log("berjalan di " + PORT)
