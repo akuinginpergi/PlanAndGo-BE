8d39dae72a15d7646d2d22b9b4e5e829b07ce98e
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-12-20 06:14:58 +0000

    feat: add arisu

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(bodyParser.json());
 app.use('/', newPlan)
 app.use('/', myPlan)
 
+app.get('/', (req, res) => {
+  res.send('<img src="https://media1.tenor.com/m/fSwZVENOqB4AAAAC/arisu-tachibana-idolmaster-cinderella-girls-u149.gif"/>')
+})
+
 app.listen(PORT, () => {
   console.log("berjalan di " + PORT)
 })
