e47466bc92990ef0a86592ff77c3c39ed2f50e6b
addthoriq <alvarro.thoriq@gmail.com>
2023-12-21 17:59:08 +0700

    fix(table-imports)

--- sql/tables.sql
+++ sql/tables.sql
@@ -8,30 +8,29 @@ CREATE TABLE public."tiket_transportasi" (
 	kelas VARCHAR(50) NOT NULL,
 	titik_kedatangan VARCHAR(100) NOT NULL,
 	kota_tiba VARCHAR(100) NOT NULL,
-	jarak VARCHAR(5),
-	harga BIGINT CHECK(harga >= 0)
+	jarak VARCHAR(30),
+	harga VARCHAR(50)
 );
 
 CREATE TABLE public."hotel" (
 	nama_hotel VARCHAR(150) NOT NULL,
-	rating REAL,
+	rating VARCHAR(10),
 	bintang VARCHAR(100),
-	harga INTEGER  CHECK(harga >= 0) NOT NULL,
+	harga VARCHAR(50) NOT NULL,
 	alamat TEXT,
 	gambar TEXT,
 	jenis_harga VARCHAR(30),
-	id VARCHAR(5) PRIMARY KEY
+	id VARCHAR(10) PRIMARY KEY
 );
 
 CREATE TABLE public."tiket_wisata" (
-	id VARCHAR(5) PRIMARY KEY,
+	id VARCHAR(10) PRIMARY KEY,
 	nama_wisata VARCHAR(100) NOT NULL,
 	deskripsi TEXT NOT NULL,
 	kategori VARCHAR(100) NOT NULL,
 	lokasi_wisata VARCHAR(100) NOT NULL,
-	harga INTEGER  CHECK(harga >= 0),
-	rating REAL, 
-	time_minutes VARCHAR(3),
+	harga VARCHAR(50),
+	rating VARCHAR(10), 
 	koordinat TEXT,
 	lat VARCHAR(30),
 	long VARCHAR(30),
@@ -43,18 +42,18 @@ CREATE TABLE public."pesananku" (
 	id VARCHAR(10) PRIMARY KEY,
 	tiket_keberangkatan VARCHAR(5) ,
 	tiket_kedatangan VARCHAR(5),
-	hotel VARCHAR(5),
+	hotel VARCHAR(10),
 	kota_asal VARCHAR(100)NOT NULL,
 	kota_tujuan VARCHAR(100)NOT NULL,
 	tanggal_berangkat DATE NOT NULL,
 	tanggal_pulang DATE NOT NULL,
-	dana INTEGER NOT NULL,
+	dana INTEGER CHECK ( dana >= 0 ) NOT NULL,
 	tema VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE public."pesanan_wisata" (
 	id VARCHAR(10) PRIMARY KEY,
-	tiket_wisata VARCHAR(5),
+	tiket_wisata VARCHAR(10),
 	pesananku VARCHAR(10),
 	CONSTRAINT fk_wisata_pesanan FOREIGN KEY (tiket_wisata) REFERENCES tiket_wisata(id),
 	CONSTRAINT fk_pesanan_wisata FOREIGN KEY (pesananku) REFERENCES pesananku(id)
